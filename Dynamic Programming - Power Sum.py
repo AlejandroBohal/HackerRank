53db90146c860c31ed18535c20851a889f3146f3
Alejandro Bohórquez <53972708+AlejandroBohal@users.noreply.github.com>
2020-07-09 07:29:06 -0500

    Create Dynamic Programming - Power Sum.py

---
+++ Dynamic Programming - Power Sum.py
@@ -0,0 +1,22 @@
+dictBottomUp = {}
+def powerSum(num,X,N):
+  if ((num,X) not in dictBottomUp.keys()):
+    if (num**N < X):
+      dictBottomUp[(num,X)] = powerSum(num+1,X,N) + powerSum(num+1,X-num**N,N)
+      return powerSum(num+1,X,N) + powerSum(num+1,X-num**N,N)
+    elif(num**N == X):
+      dictBottomUp[(num,X)] = 1
+      return 1
+    else:
+      dictBottomUp[(num,X)] = 0
+      return 0
+  else:
+    return dictBottomUp[(num,X)]
+
+def main():
+  X = int(input())
+  N = int(input())
+  print(powerSum(1,X,N))
+  
+
+main()
